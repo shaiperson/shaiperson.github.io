dc5472327edbc1fa10342b367d0a57ee28a9cb8c
Shai <bianchishai@gmail.com>
2021-12-05 20:35:04 -0300

    OK version with so-simple

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Palanca's Mission Statement",
+        "excerpt":"So What’s ML Ops All About? Nowadays, Machine Learning is ubiqutous across all manner of industries and applications. Accordingly, implementing Machine Learning solutions has become increasingly easy, with an ever-growing ecosystem of tools that facilitate its powers to software development teams who no longer require highly specialized hires in order...","categories": [],
+        "tags": ["ml-ops","tech","palanca"],
+        "url": "http://localhost:4000/2021/11/16/palanca-mission-statement.html"
+      },{
+        "title": "ML Ops: A Fictional Case Study - Chapter I",
+        "excerpt":"Table of Contents The case The product The implementation The problem An initial solution What about the next time? What about new algorithms? Enter ML Ops Palanca and the Next ChapterToday I’d like to walk you through how the incorporation of ML Ops practices might be of great assistance to...","categories": [],
+        "tags": ["ml-ops","tech"],
+        "url": "http://localhost:4000/2021/12/04/ml-ops-fict-1.html"
+      }]
--- about.markdown
+++ about.markdown
@@ -4,15 +4,4 @@ title: About
 permalink: /about/
 ---
 
-This is the base Jekyll theme. You can find out more info about customizing your Jekyll theme, as well as basic Jekyll usage documentation at [jekyllrb.com](https://jekyllrb.com/)
-
-You can find the source code for Minima at GitHub:
-[jekyll][jekyll-organization] /
-[minima](https://github.com/jekyll/minima)
-
-You can find the source code for Jekyll at GitHub:
-[jekyll][jekyll-organization] /
-[jekyll](https://github.com/jekyll/jekyll)
-
-
-[jekyll-organization]: https://github.com/jekyll
+Shai studied computer science at the Univeristy of Buenos Aires. He completed his Master's Thesis in Computer Vision working on 3D Surface Reconstruction algorithms. As a research intern, he collaborated in Computer Vision research at GRUPIVIS, FCEN, UBA. After working in back-end web development, ML algorithms and ML Ops in several startups, he turned his full attention to ML entrepreneurship.
